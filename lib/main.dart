e4b97823d1c323cdc99ca5c08e74cb00f2095c0b
Zaed Abdullah <z4ed.thalib123@gmail.com>
2022-02-27 22:50:43 +0700

    Add scroll view

--- lib/main.dart
+++ lib/main.dart
@@ -36,163 +36,165 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text("BERITA TERBARU"),
-                  style: ElevatedButton.styleFrom(
-                    onPrimary: Colors.black87,
-                    primary: const Color.fromARGB(255, 250, 250, 250),
-                    // minimumSize: const Size.fromHeight(30),
-                    fixedSize: Size(double.infinity, 3),
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(2)),
-                    ),
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text("PERTANDINGAN HARI INI"),
-                  style: ElevatedButton.styleFrom(
-                    onPrimary: Colors.black87,
-                    primary: Color.fromARGB(255, 250, 250, 250),
-                    minimumSize: const Size(88, 36),
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(2)),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.blue, width: 3),
-              ),
-              child: Column(children: <Widget>[
-                Row(
-                  children: [
-                    Expanded(
-                      child: Image.asset("/img/MU_Varane.png",
-                          fit: BoxFit.fitWidth),
-                    ),
-                  ],
-                ),
-                Container(
-                height: 40,
-                
-                child: 
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Lagi, MU imbang lawan jalur degradasi',
-                      style: GoogleFonts.roboto(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.w500,
+      body: SingleChildScrollView(child: 
+        Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text("BERITA TERBARU"),
+                    style: ElevatedButton.styleFrom(
+                      onPrimary: Colors.black87,
+                      primary: const Color.fromARGB(255, 250, 250, 250),
+                      // minimumSize: const Size.fromHeight(30),
+                      fixedSize: Size(double.infinity, 3),
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(2)),
                       ),
                     ),
-                  ],
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text("PERTANDINGAN HARI INI"),
+                    style: ElevatedButton.styleFrom(
+                      onPrimary: Colors.black87,
+                      primary: Color.fromARGB(255, 250, 250, 250),
+                      minimumSize: const Size(88, 36),
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(2)),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blue, width: 3),
                 ),
-                ),
-                Container(
-                  decoration: BoxDecoration(color: Colors.amber),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                child: Column(children: <Widget>[
+                  Row(
                     children: [
-                      Text('Transfer'),
+                      Expanded(
+                        child: Image.asset("/img/MU_Varane.png",
+                            fit: BoxFit.fitWidth),
+                      ),
                     ],
                   ),
-                )
-              ]),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 10),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.green, width: 3),
+                  Container(
+                  height: 40,
+                  
+                  child: 
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Lagi, MU imbang lawan jalur degradasi',
+                        style: GoogleFonts.roboto(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(color: Colors.amber),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Transfer'),
+                      ],
+                    ),
+                  )
+                ]),
               ),
-              child: Column(children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Container(
-                      height: 90,
-                      child: Expanded(
-                        child: Image.asset(
-                          "/img/chelsea.jpeg",
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                    Padding(padding: EdgeInsets.all(10), child: 
-                      Expanded(
-                        child: Text(
-                        'RESMI: Taipan Rusia Roman Abramovich Lepas \nChelsea'
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Container(
-                padding: EdgeInsets.all(10),
+              Container(
+                margin: EdgeInsets.only(top: 10),
                 decoration: BoxDecoration(
                   border: Border.all(color: Colors.green, width: 3),
                 ),
-                child: Row(
-                  children: [
-                    Text('Barcelona'),
-                    Text(',  '),
-                    Text('Feb 13 2022'),
-                  ],
-                ),
-                ),
-              ]),
-            ),
-
-            Container(
-              margin: EdgeInsets.only(top: 10),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.green, width: 3),
+                child: Column(children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: 90,
+                        child: Expanded(
+                          child: Image.asset(
+                            "/img/chelsea.jpeg",
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      Padding(padding: EdgeInsets.all(10), child: 
+                        Expanded(
+                          child: Text(
+                          'RESMI: Taipan Rusia Roman Abramovich Lepas \nChelsea'
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.green, width: 3),
+                  ),
+                  child: Row(
+                    children: [
+                      Text('Barcelona'),
+                      Text(',  '),
+                      Text('Feb 13 2022'),
+                    ],
+                  ),
+                  ),
+                ]),
               ),
-              child: Column(children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Container(
-                      height: 90,
-                      child: Expanded(
-                        child: Image.asset(
-                          "/img/chelsea.jpeg",
-                          fit: BoxFit.cover,
+
+              Container(
+                margin: EdgeInsets.only(top: 10),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.green, width: 3),
+                ),
+                child: Column(children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: 90,
+                        child: Expanded(
+                          child: Image.asset(
+                            "/img/chelsea.jpeg",
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       ),
-                    ),
-                    Padding(padding: EdgeInsets.all(10), child: 
-                      Expanded(
-                        child: Text(
-                        'RESMI: Taipan Rusia Roman Abramovich Lepas \nChelsea'
+                      Padding(padding: EdgeInsets.all(10), child: 
+                        Expanded(
+                          child: Text(
+                          'RESMI: Taipan Rusia Roman Abramovich Lepas \nChelsea'
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text('Barcelona'),
-                    Text(',  '),
-                    Text('Feb 13 2022'),
-                  ],
-                ),
-              ]),
-            )
-          ],
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text('Barcelona'),
+                      Text(',  '),
+                      Text('Feb 13 2022'),
+                    ],
+                  ),
+                ]),
+              )
+            ],
+          ),
         ),
       ),
     );
